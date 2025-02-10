221191b7b9882a4a906b27c2910f31f42448bc40
Mihouk <arbi_14@mail.ru>
2025-02-10 23:11:33 +0300

    Fix code and Create models,html,admin,forms,views for users

--- SendMailService/admin.py
+++ SendMailService/admin.py
@@ -1,3 +1,94 @@
+from .models import UserMail, Message, Mailing, MailingAttempt
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(UserMail)
+class UserMailAdmin(admin.ModelAdmin):
+    """Отображает модели получателей рассылки в админке"""
+
+    list_display = (
+        "id",
+        "email",
+        "fullname",
+        "comment",
+    )
+    list_filter = (
+        "email",
+        "fullname",
+    )
+    search_fields = ("id", "email", "fullname")
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    """Отображает модели письма в админке"""
+
+    list_display = (
+        "id",
+        "head_letter",
+        "body_letter",
+    )
+    list_filter = (
+        "id",
+        "head_letter",
+    )
+    search_fields = (
+        "id",
+        "head_letter",
+    )
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    """Отображает модели рассылки в админке"""
+
+    list_display = (
+        "id",
+        "date_start",
+        "date_end",
+        "status",
+        "message",
+    )
+    list_filter = (
+        "id",
+        "date_start",
+        "date_end",
+        "status",
+        "message",
+        "user_mail",
+    )
+    search_fields = (
+        "id",
+        "date_start",
+        "date_end",
+        "status",
+        "message",
+        "user_mail",
+    )
+
+
+@admin.register(MailingAttempt)
+class MailingAttemptAdmin(admin.ModelAdmin):
+    """Отображает модели попытки рассылки в админке"""
+
+    list_display = (
+        "id",
+        "datetime_attempt",
+        "status",
+        "mail_response",
+        "mailing",
+    )
+    list_filter = (
+        "id",
+        "datetime_attempt",
+        "status",
+        "mail_response",
+        "mailing",
+    )
+    search_fields = (
+        "id",
+        "datetime_attempt",
+        "status",
+        "mail_response",
+        "mailing",
+    )
